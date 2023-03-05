5c5dda19d1455da9d17c94be1152710fe41f628d
Dicky Nursalim <blackdesirewx9@gmail.com>
2023-03-05 16:04:52 +0700

    module export nodejs

---
+++ app.js
@@ -0,0 +1,4 @@
+var a = "100";
+var module_test = require('./module/test_module');
+console.log(a);
+console.log(module_test)
---
+++ module/test_module.js
@@ -0,0 +1,2 @@
+var object_test = {"key1" : "value1" , "key2" : "value2"}
+exports.object_data = object_test
